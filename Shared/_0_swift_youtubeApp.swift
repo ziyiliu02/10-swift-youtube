d082ed6ae80bcac68eee9e5219557c5ca6326a7c
Liu Ziyi <ziyi.liu02@gmail.com>
2023-07-05 09:35:25 +0800

    Initial Commit

---
+++ Shared/_0_swift_youtubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0_swift_youtubeApp.swift
+//  Shared
+//
+//  Created by Liu Ziyi on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct _0_swift_youtubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
